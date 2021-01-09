ca0a942ad5e2434565cc3e9da8dbb540fb1cebec
Patrick Leisinger <patrick.leisinger@gmail.com>
2021-01-09 13:06:23 +0100

    [FIX] add initial scale 1

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,7 @@
 
 <html lang="de">
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="utf-8">
   <title>Prototyping SoaPatrickNine</title>
   <meta name="description" content="">
